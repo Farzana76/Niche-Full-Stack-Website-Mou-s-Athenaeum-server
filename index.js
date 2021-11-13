7284f22c99a8bffbef51f83721ebe2548a54b44a
farzana76 <farzanamou773.com>
2021-11-13 11:04:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT || 5000
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ppta4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+        await client.connect();
+        
+        const database = client.db('mous_athenaeum');
+        const productCollection = database.collection('products');
+        const ordersCollection = database.collection('orders');
+        const usersCollection = database.collection('users');
+
+        // // GET API
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        //POST API
+        app.post('/products', async(req, res) =>{
+            const product = req.body;
+            console.log("hit the post api", product);
+
+            const result = await productCollection.insertOne(product);
+            console.log(result);
+            res.json(result);
+        });
+
+        // // GET my orders API
+        // app.get('/orders', async (req, res) => {
+        //     const cursor = ordersCollection.find({});
+        //     const orders = await cursor.toArray();
+        //     res.send(orders);
+        // });
+
+        // //Orders API
+        // app.post('/orders', async(req, res) =>{
+        //     const order = req.body;
+        //     const result = await ordersCollection.insertOne(order);
+        //     res.json(result);
+        // });
+
+        // // DELETE API of my orders
+        // app.delete('/orders/:id', async (req, res) => {
+        //     // console.log('hit the post api', id);
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await ordersCollection.deleteOne(query);
+        //     res.json(result);
+        //     // res.json("delete");
+        // });
+
+        // //UPDATE API
+        // app.put('/orders/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const updatedOrder = req.body;
+        //     const filter = { _id: ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updateDoc = {
+        //         $set: {
+        //             status: 'Approved'
+        //         },
+        //     };
+        //     const result = await ordersCollection.updateOne(filter, updateDoc, options);
+        //     res.json(result);
+        // })
+
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
